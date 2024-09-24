2736acda2b21d1e09eeae0d2abff2208fb968b98
alcaldemg <alcaldemg@gmail.com>
2024-09-24 16:01:54 -0300

    Agrego solucion-click y el codigo correspondiente al punto 5

--- eventos.js
+++ eventos.js
@@ -1,5 +1,13 @@
+let botonSaludar = document.querySelector('button');
+
+botonSaludar.addEventListener('click', function(event) {
+    event.stopPropagation();
+    alert('¡Hola!');
+});
+
 let boton = document.getElementById('boton');
 
-boton.addEventListener('click', function() {
+boton.addEventListener('click', function(event) {
+    event.stopPropagation();
     alert('¡Hola! Soy el div');
 });
